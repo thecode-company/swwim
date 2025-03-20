174ac85a693dd1f01cc8e5c593a766636520a1a5
jammy-git <james@thecode.company>
2025-03-20 13:19:02 +0000

    Implement site security headers

--- next.config.js
+++ next.config.js
@@ -5,6 +5,39 @@ module.exports = {
     const sanityRedirects = await redirects();
     return sanityRedirects;
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self' *.sanity.io *.swimm.io; script-src 'self' 'unsafe-inline' 'unsafe-eval' *.sanity.io *.swimm.io *.google-analytics.com *.googletagmanager.com *.facebook.net *.pinimg.com *.licdn.com *.tiktok.com; style-src 'self' 'unsafe-inline' *.googleapis.com; img-src 'self' data: *.sanity.io *.giphy.com *.placedog.net *.ads.linkedin.com *.linkedin.com; font-src 'self' *.gstatic.com; connect-src 'self' *.sanity.io *.swimm.io *.google-analytics.com *.doubleclick.net *.facebook.com *.linkedin.com *.pinimg.com *.tiktok.com *.pinterest.com; frame-src 'self' *.youtube.com *.vimeo.com *.swimm.io *.mailchimp.com *.facebook.com *.pinterest.com",
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+        ],
+      },
+    ];
+  },
   images: {
     formats: ['image/webp'],
     domains: ['placedog.net', 'cdn.sanity.io'],
